b548b0e8c16b9c46cea7d0b5108ec5a94802cb71
aNNiMON <annimon119@gmail.com>
2026-06-24 12:00:38 +0300

    Initial structure, CORS and /data serving

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use axum::Router;
+use std::path::PathBuf;
+use std::{env, fs};
+use tokio::net::TcpListener;
+#[cfg(debug_assertions)]
+use tower_http::cors::{Any, CorsLayer};
+use tower_http::services::ServeDir;
+
+#[derive(Clone)]
+pub struct AppState {
+    pub data_dir: PathBuf,
+}
+
+async fn run_app() -> Result<(), Box<dyn std::error::Error>> {
+    let bind_address = env::var("BIND_ADDRESS").expect("BIND_ADDRESS not set");
+
+    let data_dir = PathBuf::from("data");
+    fs::create_dir_all(&data_dir)?;
+
+    let app_state = AppState {
+        data_dir: data_dir.clone(),
+    };
+
+    let app = Router::new().nest_service("/data", ServeDir::new(&data_dir));
+
+    #[cfg(debug_assertions)]
+    let app = app.layer(
+        CorsLayer::new()
+            .allow_origin(Any)
+            .allow_methods(Any)
+            .allow_headers(Any),
+    );
+
+    let app = app.with_state(app_state);
+
+    println!("Starting server on {}", bind_address);
+    let listener = TcpListener::bind(&bind_address).await?;
+    axum::serve(listener, app).await?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    if let Err(e) = run_app().await {
+        eprintln!("Error starting app: {}", e);
+        std::process::exit(1);
+    }
+}
